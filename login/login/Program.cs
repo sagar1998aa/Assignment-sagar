997c145bf5f7d0cf656e2a71aad99e7e3a275389
Sagar Devkota <30004439@student.toiohomai.ac.nz>
2019-06-14 09:26:16 +1200

    msg

---
+++ login/login/Program.cs
@@ -0,0 +1,120 @@
+﻿/*Name =Sagar Devkota */
+/* student id=30004439*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("**************Welcome******************");
+            Console.WriteLine("The Holiday Park’s new login system ");
+
+            string n, p, r;
+            // display to user
+            Console.WriteLine("Please enter username atleast 8 character:");
+            // save input
+            n = Console.ReadLine();
+            // show output of our method:
+                Console.WriteLine($"{usernameanswer(n)}");
+            
+           
+            
+
+
+                Console.WriteLine($"{n} Please enter atleast 8 character password:");
+                p = Console.ReadLine();
+            
+
+                Console.WriteLine($" {password(p)}");
+                
+                
+
+                Console.WriteLine($"{n} Please comfirm your password should be match");
+                r = Console.ReadLine();
+           
+
+                Console.WriteLine($" {rpassword(r, p)}");
+            
+            
+
+
+            Console.WriteLine("***********Thank you************");
+            Console.WriteLine("******* you are Login*********");
+            Console.WriteLine("*******press enter to continue*********");
+            Console.ReadLine();
+        }
+
+
+        //method for username
+        static public  string usernameanswer(string n)
+        {
+           // while loop to check length:
+           while(n.Length <= 7)
+            {
+               
+
+                Console.WriteLine("Please re-enter username should be  8 character :");
+                n = Console.ReadLine();
+
+
+            }
+            return n;
+           
+        }
+       // method for password length:
+        static public string password(string p)
+        {
+
+
+            // while loop to check length:
+            while (p.Length <= 7)
+            {
+
+
+                Console.WriteLine("sorry, Please try again be sure  it should be  8 character :");
+              p   = Console.ReadLine();
+
+
+            }
+            return p;
+        }
+        //method to comfirm "is re-password same?"
+        static public string rpassword(string r, string p)
+        {
+            
+            if( r != p)
+            {
+
+
+
+                Console.WriteLine(" sorry,' Please try again', be sure that password should be match:");
+               r  = Console.ReadLine();
+
+               
+
+            }
+           
+
+            return r;
+           
+        }
+
+
+
+
+
+
+
+
+
+
+        
+    }
+    
+}
---
+++ matrix/q3/Program.cs
@@ -0,0 +1,71 @@
+﻿/*name= Sagar Devkota*/
+/*student id=30004439*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            Random rand = new Random();
+            int[,] array = new int[3, 3];
+           
+   
+            
+            int[,] matrix = new int[ 3, 3 ]{ { -2, -5,-1 }, {- 4, -2,- 5 }, { -5, -1, -3 } };
+            int[,] matrixb =new int[ 3, 3 ] { { 9, 8, 7 }, { 6, 4, 4 }, { 1, 2, 3 } };
+            Console.WriteLine("******************");
+            Console.WriteLine("Welcome to matrices");
+             Console.WriteLine("****************");
+
+            Console.WriteLine("\n the length property returns: " + matrix.Length);
+
+            //Displaying a 2d array.
+            Console.WriteLine("\n Array1:");
+           Display(matrix);
+            Console.WriteLine("\n Array2:");
+            Display(matrixb);
+          // in this added the array with together
+
+            Console.WriteLine("\nArrays added together:");
+            for (i = 0; i <= 2; i++)
+            {
+                for (j = 0; j <= 2; j++)
+                {
+                     Console.Write(matrix[i, j] + matrixb[i, j] + "  ");
+                }
+                Console.WriteLine();
+               
+
+               
+            }
+            Console.WriteLine("**************Press Enter to Continue*************");
+            Console.ReadLine();
+
+        }
+
+        public static void Display(int[,] arraya)
+        {
+            //row
+            for (int i = 0; i < arraya.GetLength(0); i++)
+            {
+                //collumn
+                for (int j = 0; j < arraya.GetLength(1); j++)
+                {
+                    Console.Write(arraya[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+           
+            Console.ReadLine();
+        }
+        
+    }
+    
+}
